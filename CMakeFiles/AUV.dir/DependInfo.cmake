9f666213ef864fdb98aacd5b836c43f06080371e
mentocz <andyaalgaard@gmail.com>
2021-06-03 23:34:28 +0200

    SLAM Poetry

---
+++ CMakeFiles/AUV.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andy/Documents/Coding/AUV/SLAM/src/main.cpp" "CMakeFiles/AUV.dir/SLAM/src/main.cpp.o" "gcc" "CMakeFiles/AUV.dir/SLAM/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- SLAM/src/main.cpp
+++ SLAM/src/main.cpp
@@ -1,10 +1,22 @@
 // Uncomment the following line if you are compiling this code in Visual Studio
 //#include "stdafx.h"
 
-#include <SDL2/SDL.h>
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
+
+#include "../include/display.hpp"
+
+using namespace cv;
+
 
 int main(int argc, char *argv[]) {
+
+    if(argc != 2)
+        return 0;
+
+    Display display;
+    display.Initialize();
+
+    display.PlayVideo(argv[1]);
+
     return 0;
 }
