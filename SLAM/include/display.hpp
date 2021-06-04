0232af6e133baef01bd5e2b2061c496dee97b2de
jonhas <jonhascolina@gmail.com>
2021-06-04 02:01:03 -0500

    refactored display2d and have it working properly

--- SLAM/include/display.hpp
+++ SLAM/include/display.hpp
@@ -3,112 +3,38 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <stdexcept>
+#include <functional>
+#include <iostream>
+#include <memory>
 
-#include <opencv4/opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 
-using namespace cv;
-class Video
-{
-public:
-    Video(char const* filename)
-    {
-        capture = VideoCapture(filename);
-    }
+namespace Slam {
 
-    Mat GetFrame()
-    {
-        Mat frame;
-        capture >> frame;
-        return frame;
+typedef struct Display2D Display2D;
+typedef struct Display3D Display3D;
+
+struct Display2D {
+
+  static int initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+      std::cerr << "Could not initialize SDL2" << '\n';
+      return -1;
     }
-private:
-    VideoCapture capture;
+    IMG_Init(IMG_INIT_PNG);
+
+    return 0;
+  }
+
+  static SDL_Texture *CVToSDL(const cv::Mat &matrix, SDL_Renderer *rend) {
+    SDL_Texture *tex =
+        SDL_CreateTexture(rend, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STATIC,
+                          matrix.cols, matrix.rows);
+    SDL_UpdateTexture(tex, nullptr, (void *)matrix.data, matrix.step1());
+    return tex;
+  }
 };
 
-struct Display 
-{
-    Display(){this->Initialize();}
-
-    void Initialize()
-    {
-
-        if(SDL_Init(SDL_INIT_VIDEO) < 0)
-            throw std::runtime_error("Error initializing SDL2");
-
-        IMG_Init(IMG_INIT_PNG);
-
-        window = SDL_CreateWindow("AUV Display", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 480, 480, SDL_WINDOW_SHOWN);
-        if(window == 0)
-            throw std::runtime_error("Could not make SDL2 Window");
-        renderer = SDL_CreateRenderer(window, -1, 0);
-    }
-
-    void Destroy()
-    {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-
-        window = 0;
-        renderer = 0;
-    }
-
-    SDL_Texture* CVToSDL(const cv::Mat& matrix)
-    {
-        SDL_Texture* tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STATIC, matrix.cols, matrix.rows);
-        SDL_UpdateTexture(tex, 0, (void*)matrix.data, matrix.step1());
-        return tex;
-    }
-
-    void HandleEvents()
-    {
-        SDL_Event e;
-        while(SDL_PollEvent(&e) != 0)
-        {
-            if(e.type == SDL_QUIT || e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE)
-            {
-                this->Destroy();
-                return;            
-            }
-        }
-        SDL_RenderClear(renderer);   
-    }
-
-    void PlayVideo(char const* fname)
-    {
-        Video video(fname);
-
-        for(;;)
-        {
-            cv::Mat frame = video.GetFrame();
-            if(frame.empty()) break;
-            this->DrawFrame(CVToSDL(frame));
-        }
-    }
-
-    SDL_Texture* LoadImage(char const* filename)
-    {
-        SDL_Surface* surface = IMG_Load(filename);
-        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-        return texture;
-    }
-
-    void DrawFrame(SDL_Texture* texture)
-    {
-        if(window == 0 || renderer == 0) return;
-
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture,0,0);
-        SDL_RenderPresent(renderer);
-    }
-
-public:
-    SDL_Window* window = 0;
-    SDL_Renderer* renderer = 0;
-};
-
-
-
-
+} // namespace Slam
 #endif
--- SLAM/src/main.cpp
+++ SLAM/src/main.cpp
@@ -5,18 +5,65 @@
 
 #include "../include/display.hpp"
 
-using namespace cv;
-
-
 int main(int argc, char *argv[]) {
 
-    if(argc != 2)
-        return 0;
+  if (argc != 2) {
+    std::cerr << "Required input file for video testing" << '\n';
+    return -1;
+  }
 
-    Display display;
-    display.Initialize();
+  if (!argv[1]) {
+    std::cerr << "Invalid input file" << '\n';
+    return -1;
+  }
 
-    display.PlayVideo(argv[1]);
+  auto capture = cv::VideoCapture(argv[1]);
 
-    return 0;
+  if (!capture.isOpened()) {
+    std::cerr << "Unable to open input file" << '\n';
+    return -1;
+  }
+
+  if (Slam::Display2D::initialize() != 0) {
+    std::cerr << "Invalid initialization" << '\n';
+    return -1;
+  }
+
+  auto win = std::unique_ptr<SDL_Window, std::function<void(SDL_Window *)>>(
+      SDL_CreateWindow("AUV Slam", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 980, 540, SDL_WINDOW_SHOWN),
+      SDL_DestroyWindow);
+
+  if (!win) {
+    std::cerr << "Invalid window construction" << '\n';
+    return -1;
+  }
+
+  auto rend =
+      std::unique_ptr<SDL_Renderer, std::function<void(SDL_Renderer *)>>(
+          SDL_CreateRenderer(win.get(), -1, 0), SDL_DestroyRenderer);
+
+  if (!rend) {
+    std::cerr << "Invalid renderer construction" << '\n';
+    return -1;
+  }
+
+  cv::Mat frame;
+  while (capture.isOpened()) {
+    bool is_success = capture.read(frame);
+    if (!is_success)
+      break;
+    if (!rend)
+      break;
+    SDL_RenderClear(rend.get());
+    SDL_RenderCopy(rend.get(), Slam::Display2D::CVToSDL(frame, rend.get()), 0,
+                   0);
+    SDL_RenderPresent(rend.get());
+    SDL_Delay(20);
+  }
+
+  SDL_Quit();
+  capture.release();
+  cv::destroyAllWindows();
+  return 0;
 }
